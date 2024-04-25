ca8e570293bcb5ed2594e40b0208303bd1127c52
jishnupunathil <jishnupunathil000@gmail.com>
2024-04-25 16:09:48 +0530

    logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import { Routes } from 'react-router-dom'
+import { Navigate,Route,Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
-import { Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext'
 
 
 function App() {
+  const {authUser}=useAuthContext()
+  console.log("🚀 ~ App ~ authUser:", authUser)
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
     <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/signup' element={<Signup/>}/>
+    <Route path='/' element={authUser ? <Home/>: <Navigate to={"/login"}/>}/>
+    <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login/>}/>
+    <Route path='/signup' element={authUser ? <Navigate to='/' /> : <Signup/>}/>
     </Routes>
     <Toaster/>
     </div>
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import toast from 'react-hot-toast'
+import { useAuthContext } from '../context/AuthContext'
 
 const useSignup = () => {
+    const {setAuthUser}=useAuthContext()
   const [loading,setLoading]=useState(false)
 
   const signup=async(inputs)=>{
@@ -11,14 +13,20 @@ const useSignup = () => {
     
     setLoading(true)
     try{
-        const res = await fetch("http://localhost:5000/api/auth/signup", {
+        const res = await fetch("/api/auth/signup", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(inputs),
         });
 
         const data = await res.json();
-        console.log(data);
+        if(data.error){
+            throw new Error(data.error)
+        }
+        //localstorage
+        localStorage.setItem("chat-user",JSON.stringify(data))
+        //context
+        setAuthUser(data)
 
     }catch(error){
         toast.error(error.message)
@@ -50,6 +58,7 @@ const handleInputError=(inputs)=>{
 
     if(password.length<6){
         toast.error("password must be atleast 6 character")
+        return false
     }
 
 
